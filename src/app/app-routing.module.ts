c8b6fab2d28b617cf287fad6d7377b06ae865070
Jakub <jakub.mitulski@gmail.com>
2018-07-01 10:21:57 +0200

    Added error page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import {CommonModule} from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
 import {IndexComponent} from './home/index/index.component';
 import {ContactComponent} from './home/contact/contact.component';
+import {ErrorComponent} from './error/error.component';
 
 export const appRoutes: Routes = [
   {path: '', component: IndexComponent},
-  {path: 'contact', component: ContactComponent}
+  {path: 'contact', component: ContactComponent},
+  {path: '**', component: ErrorComponent}
 ];
 
 @NgModule({
